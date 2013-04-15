feb38609c7c1f8f62947d8fe88c5f1e81d8ac775
Lars Greiss <kufikugel@googlemail.com>
2013-04-15 18:37:51 +0200

    display-caf: sony devices do include kernel headers
    in another way....
    
    Change-Id: I20205f8d470e92476b305243b0e4f89bd200330a

--- common.mk
+++ common.mk
@@ -37,6 +37,8 @@ ifeq ($(TARGET_USES_QCOM_BSP),true)
     common_flags += -DQCOM_BSP
 endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifneq ($(BOARD_VENDOR),sony)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+endif
 endif
