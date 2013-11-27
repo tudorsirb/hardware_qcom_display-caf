CyanogenMod/android_hardware_qcom_display

hwc: Enable rotator during resolution change.
MDP VG pipe framesize register is not double bufferred.
It introduces corrupted frame during resolution change especially when
rotator is not used. Enable rotator to handle the transition case.

Change-Id: Ifec55fee0c762dfb12a09580b80dca6f051a1c86
Acked-by: Ramkumar Radhakrishnan <ramkumar@codeaurora.org>

msm8960/libhwcomposer/hwc_utils.h
@@ -321,6 +321,7 @@ struct hwc_context_t {
     qhwc::LayerRotMap *mLayerRotMap[MAX_DISPLAYS];
     qhwc::MDPComp *mMDPComp[MAX_DISPLAYS];
     qhwc::CablProp mCablProp;
+    overlay::utils::Whf mPrevWHF[MAX_DISPLAYS];
 
     //Securing in progress indicator
     bool mSecuring;

tudorsirb/hardware_qcom_display-caf

libhwcomposer/hwc_utils.h
@@ -392,6 +392,7 @@ struct hwc_context_t {
     qhwc::LayerProp *layerProp[HWC_NUM_DISPLAY_TYPES];
     qhwc::MDPComp *mMDPComp[HWC_NUM_DISPLAY_TYPES];
     qhwc::CablProp mCablProp;
+    overlay::utils::Whf mPrevWHF[HWC_NUM_DISPLAY_TYPES];
 
     // No animation on External display feature
     // Notifies hwcomposer about the device orientation before animation.